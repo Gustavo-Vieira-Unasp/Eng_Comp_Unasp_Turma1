dfc7a5163992dc084a32c5d5fb23c12956eedb34
Gustavo.Vieira <gustavofav1804@gmail.com>
2025-02-20 20:29:18 -0300

    again

---
+++ semestre3/dudarts/UnaspApp/models/Cachorro.cs
@@ -0,0 +1,15 @@
+namespace Modelos
+public class Cachorro {
+    public string nome;
+    public string raca;
+    public string idade;
+
+    public string Latir() {
+        if (this.idade < 5) {
+            return "auauaauauauauauauauauauaua"
+        }
+        else {
+            "all"
+        }
+    }
+}
