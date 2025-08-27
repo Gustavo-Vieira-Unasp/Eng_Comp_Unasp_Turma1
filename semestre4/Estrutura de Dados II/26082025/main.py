9409587cc505fffb74128b1260fe6082341030eb
Gustavo-Vieira-Unasp <fonseca_vieira@educadventista.org>
2025-08-27 00:45:25 -0300

    Recursivo II - Estrutura de Dados II

--- semestre4/Estrutura de Dados II/26082025/main.py
+++ semestre4/Estrutura de Dados II/26082025/main.py
@@ -1,4 +1,4 @@
-from modules.recursivo import k
+from modules.recursivo import k, k_complexo
 
 PATH = r"D:\codes\git-turma\Eng_Comp_Unasp_Turma1\semestre4\Estrutura de Dados II\26082025\diretorio"
 
